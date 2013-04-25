8698bc09957e80926a1dc57dc476bda77ede39cf
René Mujica Moreau <rene.mujica.m@gmail.com>
2013-04-25 11:25:37 -0300

    probando si estan up los hosts

--- scripts/basic.py
+++ scripts/basic.py
@@ -3,10 +3,10 @@ from scripts.helper import *
 
 @task
 def ejecutar(com):
-  if _is_host_up(env.host, int(env.port)) is True:
+  if helper._is_host_up(env.host, int(env.port)) is True:
     run(com)
 
 @task
 def copiar(archivo, destino):
-  if _is_host_up(env.host, int(env.port)) is True:
+  if helper._is_host_up(env.host, int(env.port)) is True:
     put(archivo, destino)
