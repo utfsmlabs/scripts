7a8febdaa81526052b3e1d1b6098f20b57229279
René Mujica Moreau <rene.mujica.m@gmail.com>
2013-04-25 11:36:16 -0300

    helper now a decorator

--- scripts/helper.py
+++ scripts/helper.py
@@ -2,13 +2,12 @@ import paramiko
 import socket
 
 def ignore_offline(fn):
-    def wrapped():
-        original_timeout = getdefaulttimeout()
-        setdefaulttimeout(3)
-        try:
-            Transport((env.host, int(env.port)))
-            return fn()
-        except:
-            print "The following host appears to be offline: " + env.host
-        setdefaulttimeout(original_timeout)
+    original_timeout = getdefaulttimeout()
+    setdefaulttimeout(3)
+    try:
+        Transport((env.host, int(env.port)))
+        return fn()
+    except:
+        print "The following host appears to be offline: " + env.host
+    setdefaulttimeout(original_timeout)
     return wrapped
