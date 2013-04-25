a6d6ef8779a47730cf5a2b6bbac78df4f5880362
René Mujica Moreau <rene.mujica.m@gmail.com>
2013-04-25 12:15:19 -0300

    bugfixing

--- scripts/helper.py
+++ scripts/helper.py
@@ -1,8 +1,10 @@
+from __future__ import print_function
+
 def _output_status(failed, success):
-  print "Ejecucion fallada en: "
+  print("Ejecucion fallada en: ")
   for pc in failed:
     print(pc, end=" ")
-  print "--"
-  print "Ejecucion exitosa en: "
+  print("--")
+  print("Ejecucion exitosa en: ")
   for pc in success:
     print(pc, end=" ")
