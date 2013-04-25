4b6326cefb4efb485208b41fef2abd23278fa754
René Mujica Moreau <rene.mujica.m@gmail.com>
2013-04-25 11:44:39 -0300

    bugfixing

--- scripts/helper.py
+++ scripts/helper.py
@@ -3,12 +3,13 @@ import socket
 from fabric.api import *
 
 def ignore_offline(fn):
-    original_timeout = socket.getdefaulttimeout()
-    socket.setdefaulttimeout(3)
-    try:
-        paramiko.Transport((env.host, int(env.port)))
-        return fn()
-    except:
-        print "The following host appears to be offline: " + env.host
-    socket.setdefaulttimeout(original_timeout)
+    def wrapped():
+        original_timeout = socket.getdefaulttimeout()
+        socket.setdefaulttimeout(3)
+        try:
+            paramiko.Transport((env.host, int(env.port)))
+            return fn()
+        except:
+            print "The following host appears to be offline: " + env.host
+        socket.setdefaulttimeout(original_timeout)
     return wrapped
