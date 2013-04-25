f6b2da761bc3a436b1bd53198b6003a0c1b9fe9f
René Mujica Moreau <rene.mujica.m@gmail.com>
2013-04-25 11:50:50 -0300

    bugfixing

--- scripts/basic.py
+++ scripts/basic.py
@@ -2,11 +2,13 @@ from fabric.api import *
 from scripts import helper
 
 @task
-@helper.ignore_offline
+@parallel
+#@helper.ignore_offline
 def ejecutar(com):
   run(com)
 
 @task
-@helper.ignore_offline
+@parallel
+#@helper.ignore_offline
 def copiar(archivo, destino):
   put(archivo, destino)
